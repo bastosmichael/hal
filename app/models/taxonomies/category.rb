55361269ce267959c3cd0585b9d245dc2dac2b3d
Michael Bastos <bastosmichael@gmail.com>
2014-04-08 07:08:47 -0700

    Updating Pricels specific code

--- app/models/taxonomies/category.rb
+++ app/models/taxonomies/category.rb
@@ -2,6 +2,8 @@ class Category < Taxonomy
   # A category is a type of custom Taxonomy
   has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
   belongs_to :parent_category, :class_name => "Category"
+  has_and_belongs_to_many :items
+  has_and_belongs_to_many :listings
   validates_uniqueness_of :name
   acts_as_followable
   default_scope order('name ASC')
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,14 @@ RailsStripeMembershipSaas::Application.routes.draw do
 
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
-
+      resources :items
+      resources :listings
+      resources :follows
+      resources :admin
+      get 'get_follow_urls' => 'admin#get_follow_urls'
+      get 'get_all_urls' => 'admin#get_all_urls'
+      get 'remove_listing_fields' => 'admin#remove_listing_fields'
+      get 'match_listings' => 'admin#match_listings'
     end
   end
 
@@ -34,4 +41,16 @@ RailsStripeMembershipSaas::Application.routes.draw do
   resources :categories, :only => [:index, :show]
   resources :taxonomies, :only => [:index]
   resources :tags, :only => [:index]
+
+  resources :organizations, :only => [:index]
+  resources :items, :only => [:index]
+  resources :listings, :only => [:index]
+  resources :unknowns, :only => [:index]
+
+  resources :listings do
+    member do
+      get :follow
+      get :unfollow
+    end
+  end
 end
